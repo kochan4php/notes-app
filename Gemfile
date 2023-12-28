9d42b4b4bb843af21f23ef1f3e57def95a022a52
kochan4php <aprodeosubarno@gmail.com>
2023-12-28 23:32:48 +0700

    part 2

--- Gemfile
+++ Gemfile
@@ -5,3 +5,5 @@ gem 'rubocop', '~> 1.59'
 gem 'sinatra', '~> 3.1'
 gem 'sinatra-contrib', '~> 3.1'
 gem 'solargraph', '~> 0.50.0'
+
+gem 'bcrypt', '~> 3.1'
--- routes/auth.route.rb
+++ routes/auth.route.rb
@@ -1,3 +1,9 @@
+require 'bcrypt'
+
+require_relative '../models/user'
+
+user = User.new
+
 get '/login' do
   redirect to '/notes' if session[:user_id]
   erb :"auth/login"
@@ -8,8 +14,39 @@ get '/register' do
   erb :"auth/register"
 end
 
+post '/register' do
+  redirect to '/notes' if session[:user_id]
+
+  username = params[:username]
+  email = params[:email]
+  password = BCrypt::Password.create(params[:password])
+
+  user.create({ username: username, email: email, password: password })
+  session[:success_message] = 'Registered success, please login'
+  redirect to '/login'
+end
+
 post '/login' do
+  redirect to '/notes' if session[:user_id]
+
+  email = params[:email]
+  password = params[:password]
+
+  result = user.get_one({ email: email })
+
+  result&.each do |row|
+    if BCrypt::Password.new(row['password']) == password
+      session[:user_id] = row['id']
+      session[:email] = row['email']
+      redirect to '/notes'
+    else
+      session[:error_message] = 'Wrong password'
+      redirect to '/login'
+    end
+  end
 end
 
 post '/logout' do
+  session.clear
+  redirect to '/login'
 end
